127af2b2577cc6f5c8f6ba8c92f073570c21c586
M1rzohidovich <aasadov998@gmail.com>
2024-06-26 14:10:18 +0100

    update

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ logger = logging.getLogger(__name__)
 
 # O'zingizning tokeningizni bu yerga yozing
 TOKEN = '7332054464:AAHXI3RPCoCr9XSHrMwX7J9ApdyM8t5DKig'
-ADMIN_USER_ID = '6752438642'
-REQUIRED_CHANNELS = ['@yitv111']  # Admin tomonidan kiritiladigan majburiy kanallar
+ADMIN_USER_ID = ['6752438642','586051342']
+REQUIRED_CHANNELS = ['@toshkentyoshlari_yi','@MuhriddinHamzayev']  # Admin tomonidan kiritiladigan majburiy kanallar
 
 user_votes = {}
 survey_text, survey_image_file_id = db.get_survey()
